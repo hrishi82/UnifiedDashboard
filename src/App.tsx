a9a5c7f6642692a60ebf4c1679f66819f7ec7858
Hrishikesh Barman <hrishikesh@Hrishikeshs-MacBook-Pro.local>
2025-10-02 23:29:41 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Dashboard } from "./features/Dashboard";
+
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+       test
+      </div>
+    </>
+  )
+}
+
+export default App
